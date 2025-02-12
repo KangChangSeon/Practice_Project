9f47a1ccc4f12519181636d9e6abde16d3fbc3de
KangChangSeon <rlckdtjs@naver.com>
2025-02-12 20:13:01 +0900

    commit-2

--- src/Yes/StudentDBIO.java
+++ src/Yes/StudentDBIO.java
@@ -12,45 +12,4 @@ import java.io.IOException;
 
 public abstract class StudentDBIO implements StudentIO {
 
-    protected String getFilePath() {
-        return "students.json";
-    }
-
-    protected String readFile(String filePath) {
-        File file = new File(filePath);
-        if (!file.exists()) {
-            return "파일을 읽어옵니다";
-        }
-        StringBuilder content = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                content.append(line);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return content.toString();
-    }
-
-    protected void writeFile(String filePath, String content) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            writer.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    protected Gson getGson() {
-        return new GsonBuilder().setPrettyPrinting().create();
-    }
-
-
-    protected <T> T fromJson(String json, java.lang.reflect.Type typeOfT) {
-        return getGson().fromJson(json, typeOfT);
-    }
-
-    protected String toJson(Object src) {
-        return getGson().toJson(src);
-    }
 }
